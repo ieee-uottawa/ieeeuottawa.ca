142c3f61baa8ffbeeaad432bf59729becd68cec1
Rushil Perera <webmaster@ieeeuottawa.ca>
2018-09-07 18:01:36 -0400

    Created Office Hours page

---
+++ src/pages/office-hours.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const OfficeHours = () => (
+  <div>
+    <iframe
+      src="https://calendar.google.com/calendar/embed?src=ieeeuottawa.ca_qmt88ivf3m7vetpfagn66mgl8g%40group.calendar.google.com&ctz=America%2FToronto"
+      style={{ border: 0 }}
+      width="800"
+      height="600"
+      frameBorder="0"
+      title="Office Hours Calendar"
+      scrolling="no"
+    />
+  </div>
+);
+
+export default OfficeHours;
+
