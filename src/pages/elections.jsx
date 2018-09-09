e0533830ff2b65c5b88ebfeb39223a8841c987bf
Rushil Perera <webmaster@ieeeuottawa.ca>
2018-09-09 16:39:30 -0400

    Added margins to paragraphs

--- src/pages/elections.jsx
+++ src/pages/elections.jsx
@@ -2,32 +2,32 @@ import React from 'react';
 
 const Elections = () => (
   <div>
-    <p>
+    <p className="p-margins">
       Here is the result of the annual general election! Congratulations to elected Candidates and a huge thank you to all of you who came out voted
       – we had the highest recorded voter turnout this year!
     </p>
-    <p>
+    <p className="p-margins">
       For the individuals who did not get elected, there are still many ways to stay involved with the IEEE, both locally and on an international
       scope! Everyone is welcome to attend our Annual General Meeting (open to all students) this April 7th at 4PM in SITE 5084, where we will be
       holding by-elections for vacant executive positions, and discussing other important topics –
       <a href="https://tinyurl.com/IEEEUOAGM2018">see details</a>
     </p>
-    <p>So, without further ado, here is your incoming IEEE 2018-2019 University of Ottawa Student Branch Executives.</p>
-    <p>Chair:<br />
+    <p className="p-margins">So, without further ado, here is your incoming IEEE 2018-2019 University of Ottawa Student Branch Executives.</p>
+    <p className="p-margins">Chair:<br />
       – Heemel Saha (117 Votes)<br />
       – Ivor Benderavage (101 Votes)<br />
       – No Confidence (25 Votes)<br />
       – Abstain (79 Votes)<br />
       – ELECTED CHAIR: Heemel Saha
     </p>
-    <p>
+    <p className="p-margins">
       Vice-Chair:<br />
       – Amar Jasarbasic (180 Votes)<br />
       – No Confidence (34 Votes)<br />
       – Abstain (108 Votes)<br />
       – ELECTED VICE CHAIR: Amar Jasarbasic
     </p>
-    <p>
+    <p className="p-margins">
       Treasurer:<br />
       – Robert Andrew Conrad (83 Votes)<br />
       – Sanat Nayer (70 Votes)<br />
@@ -35,7 +35,7 @@ const Elections = () => (
       – Abstain (135 Votes)<br />
       – ELECTED TREASURER: Robert Andrew Conrad
     </p>
-    <p>
+    <p className="p-margins">
       VP Academic:<br />
       – Wei Qi Hu (47 Votes)<br />
       – Michal Ridner (33 Votes)<br />
@@ -45,7 +45,7 @@ const Elections = () => (
       – Abstain (56 Votes)<br />
       – ELECTED VICE-PRESIDENT OF ACADEMIC AFFAIRS: Melody Habbouche
     </p>
-    <p>
+    <p className="p-margins">
       VP Internal:<br />
       – Sarah Rushdi Khalaf (65 Votes)<br />
       – John (Tianyu) Zhao (55 Votes)<br />
@@ -54,7 +54,7 @@ const Elections = () => (
       – Abstain (86 Votes)<br />
       – ELECTED VICE-PRESIDENT OF INTERNAL AFFAIRS: Anushka Paliwal
     </p>
-    <p>
+    <p className="p-margins">
       VP External:<br />
       – Aly Abdelrahman (137 Votes)<br />
       – Maisha Chowdhury (32 Votes)<br />
@@ -64,31 +64,31 @@ const Elections = () => (
       – Abstain (70 Votes)<br />
       – ELECTED VICE-PRESIDENT OF EXTERNAL AFFAIRS: Aly Abdelrahman
     </p>
-    <p>
+    <p className="p-margins">
       Webmaster:<br />
       – Rushil Perera (192 Votes)<br />
       – No Confidence (22 Votes)<br />
       – Abstain (108 Votes)<br />
       – ELECTED WEBMASTER: Rushil Perera
     </p>
-    <p>
+    <p className="p-margins">
       Women in Engineering Chair:<br />
       – Linda Ruswati Tang (208 Votes)<br />
       – No Confidence (23 Votes)<br />
       – Abstain (91 Votes)<br />
       – ELECTED WOMEN IN ENGINEERING AFFINITY GROUP CHAIR: Linda Ruswati Tang
     </p>
-    <p>Women in Engineering Vice-Chair:<br />
+    <p className="p-margins">Women in Engineering Vice-Chair:<br />
       – Mikeli Habash (200 Votes)<br />
       – No Confidence (19 Votes)<br />
       – Abstain (103 Votes)<br />
       – ELECTED WOMEN IN ENGINEERING AFFINITY GROUP VICE-CHAIR: Mikeli Habash
     </p>
-    <p>Online Ballots: 278<br />
+    <p className="p-margins">Online Ballots: 278<br />
       Paper Ballots: 44<br />
       Total Ballots Counted: 322
     </p>
-    <p>To all elected Candidates, congratulations again! You will all be receiving a very important email soon.</p>
+    <p className="p-margins">To all elected Candidates, congratulations again! You will all be receiving a very important email soon.</p>
   </div>
 );
 
