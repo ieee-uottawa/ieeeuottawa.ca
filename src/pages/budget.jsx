13fbdb1d3e2b5c8f0174ae42dcea3cceae6a64f0
Rushil Perera <webmaster@ieeeuottawa.ca>
2018-09-10 14:27:24 +0000

    Created Budget page

---
+++ src/pages/budget.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Budget = () => (
+  <iframe
+    width="100%"
+    title="IEEE uOttawa Budget"
+    src="https://docs.google.com/spreadsheets/d/e/2PACX-1vT2PIGHy-IYbrOnad9BOQh-eGRwY5ogl3vKgtysYeJ7BZnzUDjI7wzJ_WvHHz7UysT0EEFbHDaZ6xR6/pubhtml?widget=true&amp;headers=false"
+    style={{ height: '49.9vh' }}
+  />
+);
+
+export default Budget;
+
