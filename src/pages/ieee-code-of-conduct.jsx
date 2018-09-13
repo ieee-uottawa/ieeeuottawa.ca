807586de069c22e30d4ad7defc1d99c935b7cf8b
Rushil Perera <webmaster@ieeeuottawa.ca>
2018-09-13 09:13:31 -0400

    Created IEEE Code of Conduct page

---
+++ src/pages/ieee-code-of-conduct.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ExternalRedirect from '../components/external-redirect';
+
+const IEEECodeOfConduct = () => (
+  <ExternalRedirect
+    url="http://www.ieee.org/about/ieee_code_of_conduct.pdf"
+    urlDescription="the IEEE Code of Conduct"
+  />
+);
+
+export default IEEECodeOfConduct;
+
