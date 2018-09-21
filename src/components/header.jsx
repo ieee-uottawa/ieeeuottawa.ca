07d105efc92541d8a4cd9efc6bc7d23cc922ecd4
Rushil Perera <webmaster@ieeeuottawa.ca>
2018-09-21 18:15:46 -0400

    Fixed wrong import

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,7 +8,7 @@ import IconButton from '@material-ui/core/IconButton';
 import logo from '../images/uottawa_branch_logo-1.png';
 import { NavButton, NavDropDown } from './buttons';
 import { MenuIcon, CloseIcon } from './icons';
-import MaterialMenu from './material-components';
+import { MaterialMenu } from './material-components';
 import Link from './link';
 
 const navItems = [
