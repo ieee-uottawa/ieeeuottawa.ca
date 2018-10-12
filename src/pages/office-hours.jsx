74e69b5d684dc3c9d7a001a5803b5475bb0c019d
Rushil Perera <pererarushil1940@gmail.com>
2018-10-12 16:55:34 -0400

    Updated fall office hours (#29)

--- src/pages/office-hours.jsx
+++ src/pages/office-hours.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 
 const OfficeHours = () => (
   <img
-    src="https://instagram.fyyz1-1.fna.fbcdn.net/vp/9369a4436708ca5ec626af3ca93da12d/5C492081/t51.2885-15/e35/40419866_452999755222668_4001511463938572787_n.jpg"
+    src="https://scontent-yyz1-1.cdninstagram.com/vp/ab14411baf2acd99518ff8ef7e679eac/5C4B2248/t51.2885-15/e35/42068664_1843225942398456_3717299338623716850_n.jpg"
     alt="IEEE Office Hours Fall 2018"
     style={{ marginLeft: '25%' }}
   />
 );
 
 export default OfficeHours;
-
