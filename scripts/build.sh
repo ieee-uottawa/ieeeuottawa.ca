220989a03fbdef72b628b9f6a8f2c6d36ae4f765
Rushil Perera <pererarushil1940@gmail.com>
2018-11-07 01:00:44 -0500

    Testing build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,8 @@
 yarn build
 
-GIT_BRANCH=$(git branch | grep \* | cut -d ' ' -f2)
+GIT_BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
+echo $(git rev-parse --abbrev-ref HEAD)
+echo $GIT_BRANCH
 if [ $GIT_BRANCH = 'master' ]; then
     yarn add firebase-tools
     firebase deploy
