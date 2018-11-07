f0e5537121c400196cf36cf7b5618b2251a37807
Rushil Perera <pererarushil1940@gmail.com>
2018-11-07 01:29:11 -0500

    Fixed branch name reading logic for Travis (#51)
    * Making sure Travis always runs the build script
    
    * Testing build script
    
    * Fixed branch name reading logic

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 yarn build
 
-GIT_BRANCH=$(git branch | grep \* | cut -d ' ' -f2)
+GIT_BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
 if [ $GIT_BRANCH = 'master' ]; then
     yarn add firebase-tools
     firebase deploy
