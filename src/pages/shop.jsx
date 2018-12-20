b7d879e9a24cc1cdbfa4a5284a447170a6535c2a
Rushil Perera <pererarushil1940@gmail.com>
2018-12-20 10:09:28 -0500

    Allow for empty inputs in Shop (#64)
    * IDE changes
    
    * Changed Shop to a functional component
    
    * Updated dependencies
    
    * Empty inputs allowed in Shop

--- src/pages/shop.jsx
+++ src/pages/shop.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import Typography from '@material-ui/core/Typography';
@@ -9,19 +9,10 @@ import { ProductCard } from '../components/cards';
 
 import '../components/product-card.scss';
 
-class Shop extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { itemCount: 0 };
-  }
-
-  render() {
-    const { itemCount } = this.state;
-    return (
-      <StaticQuery
-        query={
-          graphql`
+const Shop = () => (
+  <StaticQuery
+    query={
+      graphql`
             query {
               allShopJson {
                 edges {
@@ -48,45 +39,40 @@ class Shop extends Component {
               }
             }
           `
-        }
-        render={({ allShopJson: { edges } }) => (
-          <div style={{
-            marginLeft: '16px',
-            marginRight: '16px',
-          }}
-          >
-            <Typography variant="h5" gutterBottom className="title">Shop</Typography>
-            <GridList style={{ justifyContent: 'space-evenly' }}>
-              {edges
-                .filter(({ node: { expiry } }) => !expiry || (expiry && dayjs(new Date()).isBefore(dayjs(expiry).add(1, 'day'))))
-                .map(({ node: { name, price, expiry, image, options } }) => (
-                  <GridListTile style={{
-                    width: 'inherit',
-                    height: 'inherit',
-                    padding: 'inherit',
-                    flexDirection: 'column',
-                    alignSelf: 'center',
-                  }}
-                  >
-                    <ProductCard
-                      price={price}
-                      name={name}
-                      expiry={expiry}
-                      imageURL={image}
-                      options={options}
-                      itemCount={itemCount}
-                      onChange={({ target: { value } }) => {
-                        if (value) this.setState({ itemCount: Number(value) });
-                      }}
-                    />
-                  </GridListTile>
-                ))}
-            </GridList>
-          </div>
-        )}
-      />
-    );
-  }
-}
+    }
+    render={({ allShopJson: { edges } }) => (
+      <div style={{
+        marginLeft: '16px',
+        marginRight: '16px',
+      }}
+      >
+        <Typography variant="h5" gutterBottom className="title">Shop</Typography>
+        <GridList style={{ justifyContent: 'space-evenly' }}>
+          {edges
+            .filter(({ node: { expiry } }) => !expiry || (expiry && dayjs(new Date()).isBefore(dayjs(expiry).add(1, 'day'))))
+            .map(({ node: { name, price, expiry, image, options } }) => (
+              <GridListTile style={{
+                width: 'inherit',
+                height: 'inherit',
+                padding: 'inherit',
+                flexDirection: 'column',
+                alignSelf: 'center',
+              }}
+              >
+                <ProductCard
+                  price={price}
+                  name={name}
+                  expiry={expiry}
+                  imageURL={image}
+                  options={options}
+                  itemCount={1}
+                />
+              </GridListTile>
+            ))}
+        </GridList>
+      </div>
+    )}
+  />
+);
 
 export default Shop;
