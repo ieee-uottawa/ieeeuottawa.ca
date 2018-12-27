6d358140eeaf9d48f982447ffcf5d9233ba9ba1f
Rushil Perera <pererarushil1940@gmail.com>
2018-12-27 16:25:23 -0500

    Should upload source maps to Sentry

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,3 +6,19 @@
 
 // You can delete this file if you're not using it
 
+try {
+    const SentryWebpackPlugin = require('@sentry/webpack-plugin');
+
+    module.exports.onCreateWebpackConfig = ({ actions }) => {
+        actions.setWebpackConfig({
+            plugins: [
+                new SentryWebpackPlugin({
+                    include: '/public',
+                    ignoreFile: '.sentrycliignore',
+                })
+            ]
+        })
+    };
+} catch (e) {
+    if (e.message !== 'Cannot find module \'@sentry/webpack-plugin\'') throw e;
+}
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,15 @@
+GIT_BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
+
+if [ $GIT_BRANCH = 'master' ]; then
+    yarn add @sentry/webpack-plugin
+fi
+
 yarn build
 
 if [ $? -ne 0 ]; then
     exit 1
 fi
 
-GIT_BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
 if [ $GIT_BRANCH = 'master' ]; then
     yarn add firebase-tools
     firebase deploy
