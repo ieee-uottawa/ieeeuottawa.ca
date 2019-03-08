f6bc723bb2bae2b0f1e63232de0ecd79bbd04585
Rushil Perera <pererarushil1940@gmail.com>
2019-03-08 13:05:46 -0500

    Updated Mailing List URL (#79)

--- src/pages/mailing-list-sign-up.jsx
+++ src/pages/mailing-list-sign-up.jsx
@@ -3,7 +3,7 @@ import ExternalRedirect from '../components/external-redirect';
 
 const MailingListSignUp = () => (
   <ExternalRedirect
-    url="https://docs.google.com/forms/d/e/1FAIpQLSfdkrNopk9srS4CDYP3QSpadzOcCAXcqVMzG6ryVi-CZBWRtw/viewform"
+    url="http://eepurl.com/dKDAJE"
     urlDescription="our mailing list signup form"
   />
 );
