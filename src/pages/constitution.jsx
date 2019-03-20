54f953d3560f1e62372f5c0144596a4d43bc773a
Rushil Perera <pererarushil1940@gmail.com>
2019-03-20 18:47:27 -0400

    Fixed constitution PDF url redirect

--- src/pages/constitution.jsx
+++ src/pages/constitution.jsx
@@ -3,7 +3,7 @@ import ExternalRedirect from '../components/external-redirect';
 
 import constitutionPDF from '../../static/files/IEEE-Constitution.pdf';
 
-const Constitution = () => <ExternalRedirect forceExternal={true} url={constitutionPDF} urlDescription="our constitution" />;
+const Constitution = () => <ExternalRedirect url={constitutionPDF} urlDescription="our constitution" />;
 
 export default Constitution;
 
