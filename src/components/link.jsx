d766c4ecb51eacbdc8f30f3de20ea3871c5dbfc9
Rushil Perera <pererarushil1940@gmail.com>
2019-03-22 08:57:56 -0400

    Fixed loading constitution PDF for good

--- src/components/link.jsx
+++ src/components/link.jsx
@@ -3,15 +3,15 @@ import PropTypes from 'prop-types';
 import { Link as GatsbyLink } from 'gatsby';
 import ReactGA from 'react-ga';
 
-const Link = ({ children, to, forceExternal, ...other }) => {
+const Link = ({ children, to, ...other }) => {
   // Tailor the following test to your environment.
   // This example assumes that any internal link (intended for Gatsby)
   // will start with exactly one slash, and that anything else is external.
   const internal = /^\/(?!\/)/.test(to);
-  console.log(forceExternal);
+  const isFile = /\.[0-9a-z]+$/i.test(to)
 
   // Use gatsby-link for internal links, and <a> for others
-  if (internal || !forceExternal) {
+  if (internal && !isFile) {
     return (
       <GatsbyLink to={to} {...other}>
         {children}
@@ -27,11 +27,6 @@ const Link = ({ children, to, forceExternal, ...other }) => {
 
 Link.propTypes = {
   to: PropTypes.string.isRequired,
-  forceExternal: PropTypes.bool,
 };
-
-Link.defaultProps = {
-  forceExternal: false,
-}
 
 export default Link;
