62c049012e9ff387127b08a49784bb6949ebce10
Rushil Perera <pererarushil1940@gmail.com>
2019-04-05 07:46:11 -0400

    Updated budget

--- src/pages/budget.jsx
+++ src/pages/budget.jsx
@@ -7,7 +7,7 @@ const Budget = () => (
     <iframe
       width="100%"
       title="IEEE uOttawa Budget"
-      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vT2PIGHy-IYbrOnad9BOQh-eGRwY5ogl3vKgtysYeJ7BZnzUDjI7wzJ_WvHHz7UysT0EEFbHDaZ6xR6/pubhtml?widget=true&amp;headers=false"
+      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSIRkCKPieqdaSemGMWyKDl1zysL1pBABUD9-3Zi-UpdSCX2LSNiA0g4LlXw9YwUt0Aqx5yve16zbM7/pubhtml?gid=1685439230&amp;single=true&amp;widget=true&amp;headers=false"
       style={{ height: '981px' }}
     />
   </div>
