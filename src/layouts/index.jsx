66fa4ce01977629bd5bfcf77f3bdfe958902a8cc
iDarkDuck <nevinganesan@hotmail.com>
2019-05-26 19:55:50 -0400

    Favicon Update

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -13,6 +13,7 @@ import { isDevEnvironment, isServerSideRendering } from '../util';
 
 import './index.scss';
 import logo from '../../static/images/uottawa_branch_logo-1.png';
+import favicon from '../../static/images/ieee_logo_circle.png';
 
 const store = createStore(cart);
 
@@ -46,7 +47,9 @@ const Layout = ({ children, theme = 'light', toggleTheme }) => (
       flexDirection: 'column',
     }}
     >
-      <Helmet title="IEEE uOttawa Student Branch">
+      <Helmet title="IEEE uOttawa Student Branch"
+        link={[{ rel: 'shortcut icon', type: 'image/png', href: `${favicon}` }]}
+      >
         <meta property="og:image" content={`https://ieeeuottawa.ca${logo}`} />
         <meta property="og:title" content="IEEE uOttawa Student Branch" />
         <meta property="og:description" content="The IEEE uOttawa Student Branch is the official student branch for the University of Ottawa and the official Sub-Association for ELG/CEG/SEG under the ESS. The University of Ottawa’s IEEE Student Branch was established to provide professional services to improve each student’s experience on campus. This includes accommodating students with access to up-to-date equipment, internet access, textbooks and a quiet work environment." />
