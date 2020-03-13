fbeb6c3bbb26ecfca99cd52b8a430104c50ab7a7
Nevin Wong Syum Ganesan <ngane103@uottawa.ca>
2020-03-13 11:34:46 -0400

    .

--- src/pages/Elections/ElectionPlatforms.jsx
+++ src/pages/Elections/ElectionPlatforms.jsx
@@ -1,9 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { graphql, StaticQuery } from 'gatsby';
 import { Typography } from '@material-ui/core';
 import CandidateCard from '../../components/Cards/CandidateCard/CandidateCard';
 import Title from '../../components/Titles/Title';
-import { positions } from './elections-helper';
+// import { positions } from './elections-helper';
+
+const positions = [
+    'Chair',
+    'Vice_Chair',
+    'Treasurer',
+    'VP_Social',
+    'VP_Internal',
+    'VP_Communications',
+    'VP_Academic',
+    'VP_External',
+    'WIE_Chair',
+    'WIE_Vice_Chair',
+    'Webmaster',
+    'Secretary',
+    'McNaughton_Centre_Director',
+    'Photonics_Chair',
+    'Photonics_Vice_Chair'
+];
 
 const ElectionPlatforms = () => (
     <StaticQuery
