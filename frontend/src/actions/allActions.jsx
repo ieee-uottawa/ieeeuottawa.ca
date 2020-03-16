b39796622c8a610b975ef3845610c4d074936fd5
Nevin Wong Syum Ganesan <ngane103@uottawa.ca>
2020-03-16 18:42:00 -0400

    .

--- frontend/src/actions/allActions.jsx
+++ frontend/src/actions/allActions.jsx
@@ -3,14 +3,12 @@ import axios from 'axios';
 const httpClient = axios.create();
 httpClient.defaults.timeout = 600000;
 
-const BACKEND_URL_DEV = 'http://localhost:3000/';
-
-const BACKEND_URL = BACKEND_URL_DEV;
+const BACKEND_URL = process.env.BACKEND_URL_PROD;
 
 export function getUsers() {
     return dispatch => {
         return axios
-            .get(`${BACKEND_URL}users`)
+            .get(`${BACKEND_URL}/users`)
             .then(response => {
                 dispatch({ type: 'getUsersSuccess', payload: response.data });
             })
