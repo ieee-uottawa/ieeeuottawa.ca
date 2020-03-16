94e90861aa52837fdea14f4fc02d458065a8aa44
Nevin Wong Syum Ganesan <ngane103@uottawa.ca>
2020-03-16 18:58:33 -0400

    fixed urls for platform

--- frontend/src/pages/Elections/Vote.jsx
+++ frontend/src/pages/Elections/Vote.jsx
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+
 import { Title } from '../../helpers/components';
 import { mapDispatchToProps } from '../../helpers/actions';
 import { isServerSideRendering } from '../../util';
@@ -23,6 +31,8 @@ class Vote extends Component {
         });
     }
 
+    renderForm() {}
+
     render() {
         return (
             <>
@@ -30,6 +40,36 @@ class Vote extends Component {
                     <Title variant="h5" gutterBottom className="title">
                         Vote
                     </Title>
+                    <div style={{ textAlign: 'center' }}>
+                        <FormControl
+                            component="fieldset"
+                            // className={classes.formControl}
+                        >
+                            <FormLabel component="legend">Gender</FormLabel>
+                            <RadioGroup
+                                aria-label="gender"
+                                name="gender2"
+                                // value={value}
+                                // onChange={handleChange}
+                            >
+                                <FormControlLabel
+                                    value="female"
+                                    control={<Radio color="primary" />}
+                                    label="Female"
+                                    labelPlacement="start"
+                                />
+                                <FormControlLabel
+                                    value="male"
+                                    control={<Radio color="primary" />}
+                                    label="Male"
+                                    labelPlacement="start"
+                                />
+                            </RadioGroup>
+                            <FormHelperText>
+                                labelPlacement start
+                            </FormHelperText>
+                        </FormControl>
+                    </div>
                 </>
             </>
         );
