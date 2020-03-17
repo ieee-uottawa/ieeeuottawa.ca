b2085e31fc142d705e23a0f7c3879789977a85a3
Nevin Wong Syum Ganesan <ngane103@uottawa.ca>
2020-03-17 18:33:41 -0400

    Update vote.jsx

--- frontend/src/pages/vote.jsx
+++ frontend/src/pages/vote.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 // import Vote from './Elections/Vote';
 import ForcedExternalRedirect from '../components/Routers/ForcedExternalRedirect';
-import { isFacebookApp } from '../util';
+import { isFacebookApp, isServerSideRendering } from '../util';
 
 // const browserUrlAndroid = 'googlechrome://ieeeuottawa.ca/vote';
 const browserUrliOS = 'safari://ieeeuottawa.ca/vote';
 
 const vote = () => {
+    let text = 'test';
+    if (!isServerSideRendering() && isFacebookApp()) {
+        text += '1';
+        window.location.href = 'https://ieeeuottawa.ca';
+        text += '2';
+    }
+    text += '3';
     return (
         <>
             {/* <Vote /> */}
@@ -16,6 +23,7 @@ const vote = () => {
                     urlDescription="our voting page"
                 />
             )} */}
+            {text}
             {isFacebookApp() && (
                 <ForcedExternalRedirect
                     url={browserUrliOS}
