da12a7f0cb7e0df2b1ccd3e165062dd9ee22ce23
Nevin Wong Syum Ganesan <ngane103@uottawa.ca>
2020-03-16 23:50:04 -0400

    Create vote.jsx

---
+++ frontend/src/pages/vote.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Vote from './Elections/Vote';
+
+const vote = () => {
+    return (
+        <>
+            <Vote />
+        </>
+    );
+};
+
+export default vote;
