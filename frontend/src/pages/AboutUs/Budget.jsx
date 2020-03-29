2acc2cf51eab06d7362e269efe635b22439cc243
Nevin Wong Syum Ganesan <ngane103@uottawa.ca>
2020-03-29 13:07:43 -0400

    Budget

--- frontend/src/pages/AboutUs/Budget.jsx
+++ frontend/src/pages/AboutUs/Budget.jsx
@@ -12,12 +12,20 @@ const Budget = () => (
         >
             {translate('Budget')}
         </Title>
-        <iframe
+        <div style={{ textAlign: 'center' }}>
+            <iframe
+                width="90%"
+                title="IEEE uOttawa Budget"
+                src="https://docs.google.com/spreadsheets/d/1YQLuyiUYRCE4wHIv9Dh1Suz8e-ipuaIMUAvd8U_DRLk/edit#pubhtml?gid=1846312074&amp;single=true&amp;widget=true&amp;headers=false"
+                style={{ height: '981px' }}
+            />
+        </div>
+        {/* <iframe
             width="100%"
             title="IEEE uOttawa Budget"
             src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSIRkCKPieqdaSemGMWyKDl1zysL1pBABUD9-3Zi-UpdSCX2LSNiA0g4LlXw9YwUt0Aqx5yve16zbM7/pubhtml?gid=1685439230&amp;single=true&amp;widget=true&amp;headers=false"
             style={{ height: '981px' }}
-        />
+        /> */}
     </div>
 );
 
