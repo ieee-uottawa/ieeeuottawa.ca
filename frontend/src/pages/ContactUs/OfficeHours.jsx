75d9f33d52233b30429125170e091e1a4bb4f084
Ryan Fleck <Ryan.Fleck@protonmail.com>
2021-01-09 15:09:33 -0700

    Remove incorrect html table.

--- frontend/src/pages/ContactUs/OfficeHours.jsx
+++ frontend/src/pages/ContactUs/OfficeHours.jsx
@@ -41,29 +41,6 @@ class OfficeHours extends Component {
                 <Title variant="h5" gutterBottom className="title">
                     {translate('Office Hours')}
                 </Title>
-                <table style="width:100%">
-                    <tr>
-                        <th>Firstname</th>
-                        <th>Lastname</th>
-                        <th>Age</th>
-                    </tr>
-                    <tr>
-                        <td>Jill</td>
-                        <td>Smith</td>
-                        <td>50</td>
-                    </tr>
-                    <tr>
-                        <td>Eve</td>
-                        <td>Jackson</td>
-                        <td>94</td>
-                    </tr>
-                </table>
-
-                {/*<img
-                    src={officeHoursImg}
-                    alt="IEEE Office Hours"
-                    style={imgStyle}
-                />*/}
             </>
         );
     }
