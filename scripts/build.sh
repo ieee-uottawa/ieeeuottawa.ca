b069a4b1a0ebc01e3c445b458d5b39c6fd01b650
kandloic <jkand011@uottawa.ca>
2021-12-27 13:59:07 -0500

    Build script pulls specific firebase-tools version

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,6 +8,6 @@ fi
 
 GIT_BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
 if [ $GIT_BRANCH = 'master' ]; then
-    yarn add firebase-tools
+    yarn add firebase-tools@9.21.0
     firebase deploy
 fi
