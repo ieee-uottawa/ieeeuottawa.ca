1f870f38dd6059b015bfcfde1616beebf60d6ff9
kandloic <jkand011@uottawa.ca>
2022-02-13 16:46:55 -0500

    Updated meeting minutes

--- frontend/src/helpers/meeting-minutes.jsx
+++ frontend/src/helpers/meeting-minutes.jsx
@@ -4,8 +4,8 @@ import pdf3 from '../../static/files/meeting-minutes/3. IEEE Executive Meeting M
 import pdf4 from '../../static/files/meeting-minutes/4. IEEE Executive Meeting Minutes (2021-11-03).pdf';
 import pdf5 from '../../static/files/meeting-minutes/5. IEEE Executive Meeting Minutes (2021-11-21).pdf';
 import pdf6 from '../../static/files/meeting-minutes/6. IEEE Executive Meeting Minutes (2021-12-01).pdf';
-// import pdf6 from '../../static/files/meeting-minutes/3.1 IEEE Executive Meeting Minutes .pdf';
-// import pdf7 from '../../static/files/meeting-minutes/4. IEEE Executive Meeting Agenda (Sept 16th).pdf';
+import pdf7 from '../../static/files/meeting-minutes/7. IEEE Executive Meeting Minutes (2022-01-23).pdf';
+import pdf8 from '../../static/files/meeting-minutes/8. IEEE Executive Meeting Minutes (2022-02-02).pdf';
 // import pdf8 from '../../static/files/meeting-minutes/5. IEEE Executive Meeting Agenda (Sept22nd).pdf';
 // import pdf9 from '../../static/files/meeting-minutes/6. IEEE Meeting  Agenda (Oct 2nd).pdf';
 // import pdf10 from '../../static/files/meeting-minutes/7. IEEE Meeting Agenda (October 2nd).pdf';
@@ -19,7 +19,9 @@ const pdfMap = {
     '3. IEEE Executive Meeting Minutes (2021-10-24).pdf': pdf3,
     '4. IEEE Executive Meeting Minutes (2021-11-03).pdf': pdf4,
     '5. IEEE Executive Meeting Minutes (2021-11-21).pdf': pdf5,
-    '6. IEEE Executive Meeting Minutes (2021-12-01).pdf': pdf6
+    '6. IEEE Executive Meeting Minutes (2021-12-01).pdf': pdf6,
+    '7. IEEE Executive Meeting Minutes (2022-01-23).pdf': pdf7,
+    '8. IEEE Executive Meeting Minutes (2022-02-02).pdf': pdf8
     // 'IEEE AGM Meeting Agenda (Sept 23rd).pdf': pdfAGM,
     // '6. IEEE Meeting  Agenda (Oct 2nd).pdf': pdf9,
     // '7. IEEE Meeting Agenda (October 2nd).pdf': pdf10,
